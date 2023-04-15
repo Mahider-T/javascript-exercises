f1e39e44edeff5a6d791d947c6c89a915f64f0da
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-04-15 12:32:24 +0300

    Reapeats string the number of times passed to it as argument

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalResult = "";
+    let length = 1;
+    if (num < 0) {
+        return  "ERROR";
+    }
+
+    while (length <= num) {
+        finalResult += string;
+        length++;
+    }
+    return finalResult;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
