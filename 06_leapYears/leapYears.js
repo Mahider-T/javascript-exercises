1697260f5b251aaa1262379331395125e7348f1c
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-04-15 14:53:21 +0300

    Check if the entered year is a leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    if (number % 4 !== 0) return false;
+    if (number % 100 === 0 && number % 400 !== 0) return false;
+    return true;
 };
 
 // Do not edit below this line
