ee83e85d33448116c55d82d016a4cfc94f495e13
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-04-15 14:13:53 +0300

    Remove an element in an array wihtout leaving a hole

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let index;
+   // delete array[index];
+   // return array.splice(index, 1);
+   //return array;
 
+   for (i = 1; i < arguments.length; i++) {
+    if (!array.includes(arguments[i])) continue;
+    index = array.indexOf(arguments[i]);
+    array = array.slice(0,index).concat(array.slice(index + 1));
+   }
+   return array;
+
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
